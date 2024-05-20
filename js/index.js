5726bf8d8908cbbc4fd6ddebecb4261f06496fdd
JeremiahID <jeremiahefe.je@gmail.com>
2024-05-20 15:30:59 +0100

    styles changes for the view port

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const Hamburger = document.querySelector('#hamburger');
+const CloseHamburger = document.querySelector('#close-hamburger');
+const Navigation = document.querySelector('#navigations');
+console.log("working")
+
+Hamburger.addEventListener('click', ()=>{
+    if(Navigation.style.display === "none"){
+        Navigation.style.display = "block";
+
+    }else{
+        Navigation.style.display = "none";
+    }
+})
+
+
+CloseHamburger.addEventListener('click', ()=>{
+    if(Navigation.style.display === "block"){
+        Navigation.style.display = "none";
+
+    }else{
+        Navigation.style.display = "none";
+    }
+})
