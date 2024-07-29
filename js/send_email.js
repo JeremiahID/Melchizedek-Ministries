d9bd751a3e68c6abee83230bbb1d328619332934
JeremiahID <jeremiahefe.je@gmail.com>
2024-07-29 16:31:57 +0100

    email config

--- js/send_email.js
+++ js/send_email.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+// console.log('EMAIL_USER:', process.env.EMAIL_USER);
+// console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
 
 // Set up the transporter
 const transporter = nodemailer.createTransport({
